5c87b1ad65b0ad9d849db5fd88d72555c16a60c7
RGreensmith <rose.greensmith@yahoo.co.uk>
2020-11-18 14:23:26 +0000

    Added the content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import { title, content } from './shellCrash.json';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1 className={title.className}>{title.content}</h1>
+    {content.map(({ type, items, content, path}) => {
+      if (type === 'list') {
+        return (
+          <ul>
+            {items.map((name) => <li>{name} </li>)}
+          </ul>
+        );
+      } else if (type === 'paragraph') {
+        return <p>{content}</p>;
+      } else if (type === 'subheading') {
+        return <h4>{content}</h4>;
+      } else if (type === 'image') {
+        import(`./assets/images/${path}`)
+        return <img src={path} alt="Picture" />;
+      }
+      return '';
+    })}
+  </div>
+);
 
 export default App;
