a59a8196d55f83b1bd3671267ed1c2f4c0171dc3
RGreensmith <rose.greensmith@yahoo.co.uk>
2020-12-08 11:24:04 +0000

    Created classification application file and incorporated it into the App js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Page from './components/page';
 import CNN from './pageDefinitions/CNN.json';
 import shellCrash from './pageDefinitions/shellCrash.json'
+import ClassificationApplication from './pageDefinitions/ClassificationApplication'
 
 const App = () => (
   <Router>
@@ -18,6 +19,9 @@ const App = () => (
         <li>
           <Link to="/shellcrash">Shell Crash</Link>
         </li>
+        <li>
+          <Link to="/ClassificationApplication">Classification Application</Link>
+        </li>
       </ul>
     </nav>
     <Switch>
@@ -26,6 +30,9 @@ const App = () => (
       </Route>
       <Route path="/shellcrash" >
         <Page description={shellCrash} />
+      </Route>
+      <Route path="/ClassificationApplication" >
+        <Page description={ClassificationApplication} />
       </Route>
     </Switch>
   </Router>
